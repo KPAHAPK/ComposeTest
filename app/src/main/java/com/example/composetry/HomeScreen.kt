2dd3e602ae58ee7a46c3a495f396c0d800c3b969
Kran <Roman444.zaharov@gmail.com>
2025-10-01 18:40:52 +0300

    lesson 10

--- app/src/main/java/com/example/composetry/HomeScreen.kt
+++ app/src/main/java/com/example/composetry/HomeScreen.kt
@@ -4,16 +4,21 @@ import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun HomeScreen(
@@ -25,15 +30,21 @@ fun HomeScreen(
     onTextChange: (String) -> Unit
 ) {
     val counterValue = click.value
-    val checkedValue = checked.value
-    Log.d("TAG", "RRRRR")
+    var checked by remember { mutableStateOf(false)}
+    Log.d("TAG", "checkedValue: $checked")
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        ClickCounter(counterValue, onCounterClick, checkedValue)
-        Checkbox(checked = checkedValue, onCheckedChange = { onCheckedChange(checkedValue) })
+//        ClickCounter(counterValue, onCounterClick, checkedValue)
+        Row(modifier = Modifier.clickable(onClick = { checked = !checked })) {
+            Checkbox(checked = checked, onCheckedChange = { checked = !checked })
+            Text("More details", fontSize = 18.sp)
+        }
+        if (checked){
+            Text(text = "ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd")
+        }
     }
 
 }
