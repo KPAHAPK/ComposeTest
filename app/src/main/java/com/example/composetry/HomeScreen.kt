298aa3bc35fa9598861f6e30013c3a6d825f6aea
Kran <Roman444.zaharov@gmail.com>
2025-10-06 14:15:09 +0300

    lesson 14

--- app/src/main/java/com/example/composetry/HomeScreen.kt
+++ app/src/main/java/com/example/composetry/HomeScreen.kt
@@ -2,52 +2,44 @@ package com.example.composetry
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun HomeScreen(
-    viewModel: HomeViewModel = viewModel()
 ) {
-    val counter by viewModel.counter
-    ClickCounter(counter, onCounterClick = viewModel::increase)
-}
-
-@Composable
-private fun ClickCounter(
-    counter: Int,
-    onCounterClick: () -> Unit
-) {
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ) {
-
-        Text(
-            text = "Clicks: $counter",
-            modifier = Modifier.clickable(onClick = { onCounterClick() })
-        )
+    Column {
+        Text(text = "Home screen")
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun ClickCounterPreview() {
-    var counter by remember { mutableStateOf(5) }
-    ClickCounter(
-        counter = counter,
-        onCounterClick = {counter++}
-    )
+fun OrdersScreen() {
+    Text(text = "Order Screen")
 }
+
+@Composable
+fun UsersScreen(
+    onUserClick: (String) -> Unit
+) {
+    Text(
+        text = "User Screen",
+        modifier = Modifier.clickable { onUserClick("asdfas") })
+}
+
+@Composable
+fun UserScreen(
+    id: String = ""
+) {
+    Text(text = id)
+}
+
+
 
 
 
--- app/src/main/java/com/example/composetry/MainActivity.kt
+++ app/src/main/java/com/example/composetry/MainActivity.kt
@@ -3,21 +3,65 @@ package com.example.composetry
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModelProvider
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        val count = mutableIntStateOf(0)
-        val checked = mutableStateOf(false)
-        val text = mutableStateOf("false")
         setContent {
-            HomeScreen(
-            )
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .statusBarsPadding()) {
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "home") {
+                    composable("home") { HomeScreen() }
+                    composable("orders") { OrdersScreen() }
+                    composable("users") { UsersScreen(
+                        onUserClick = {
+                            navController.navigate("user/$it")
+                        }
+                    ) }
+                    composable(
+                        route = "user/{id}",
+                        arguments = listOf(navArgument("id") { type = NavType.StringType })
+                    ) {
+                        val idString = it.arguments?.getString("id") ?: "null"
+                        UserScreen(idString)
+                    }
+                }
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.Bottom,
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(
+                        text = "Home",
+                        modifier = Modifier.clickable { navController.navigate("home") })
+                    Text(
+                        text = "Orders",
+                        modifier = Modifier.clickable { navController.navigate("orders") })
+                    Text(
+                        text = "Users",
+                        modifier = Modifier.clickable { navController.navigate("users") })
+                }
+            }
         }
     }
 }
