8922df850239a1f78e87fd9d9060581e57710830
rozak <roman.zakharov@homelifemanager.cloud>
2025-10-07 18:51:44 +0300

    lesson 19

--- app/src/main/java/com/example/composetry/HomeScreen.kt
+++ app/src/main/java/com/example/composetry/HomeScreen.kt
@@ -1,32 +1,27 @@
 package com.example.composetry
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
 import android.util.Log
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Slider
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.produceState
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.rememberUpdatedState
-import androidx.compose.ui.platform.LocalContext
-import androidx.core.content.ContextCompat
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.compose
+import kotlin.math.roundToInt
 
 @Composable
 fun HomeScreen(
@@ -38,40 +33,49 @@ fun HomeScreen(
         Checkbox(checked = checked1, onCheckedChange = { checked1 = it })
         Checkbox(checked = checked2, onCheckedChange = { checked2 = it })
         Checkbox(checked = checked3, onCheckedChange = { checked3 = it })
-        if (checked1){
-            val counter by produceState(initialValue = 0) {
-                awaitDispose {
-
-                }
-            }
-            Text(text = "count - $counter")
+        if (checked1) {
+            val sliderPosition = remember { mutableStateOf(1f) }
+            MySlider(sliderPosition)
+            val roundedPosition = remember { derivedStateOf { sliderPosition.value.roundToInt() } }
+            Text(text = "position ${roundedPosition}")
+            Log.d("TAG", "HomeScreen: ${roundedPosition.value}")
         }
-        if (checked2){
-            var sliderPosition by remember { mutableStateOf(1f) }
+        if (checked2) {
+            val sliderPosition = remember { mutableStateOf(1f) }
             Slider(
-                value = sliderPosition,
-                onValueChange = { sliderPosition = it },
+                value = sliderPosition.value,
+                onValueChange = { sliderPosition.value = it },
                 valueRange = 0f..10f
             )
-            TrackPostition(position = sliderPosition)
+            TrackPostition(sliderPosition)
         }
     }
 }
 
 @Composable
-fun TrackPostition(position: Float) {
-//    val postitionState = remember {
-//        mutableStateOf(position)
-//    }
-//    postitionState.value = position
+fun MySlider(sliderPosition: MutableState<Float>) {
+    Slider(
+        value = sliderPosition.value,
+        onValueChange = { sliderPosition.value = it },
+        valueRange = 0f..10f
+    )
+}
 
-    val positionState = rememberUpdatedState(position)
+@Composable
+fun TrackPostition(position: State<Float>) {
     LaunchedEffect(true) {
-        while (true){
-            delay(1000)
-            Log.d("TAG", "TrackPostition: ${positionState.value}")
+        val flow = snapshotFlow{position.value}
+        flow.collect {
+            Log.d("TAG", "TrackPostition: ${it}")
+
         }
     }
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
 }
 
 
