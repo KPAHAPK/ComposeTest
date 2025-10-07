465cfd9f63f387ba7b83cb932189d4beb0d016b1
rozak <roman.zakharov@homelifemanager.cloud>
2025-10-07 18:19:41 +0300

    lesson 18

--- app/src/main/java/com/example/composetry/HomeScreen.kt
+++ app/src/main/java/com/example/composetry/HomeScreen.kt
@@ -17,9 +17,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Slider
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.produceState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
 import kotlinx.coroutines.delay
@@ -33,56 +36,40 @@ fun HomeScreen(
         var checked2 by remember { mutableStateOf(false) }
         var checked3 by remember { mutableStateOf(false) }
         Checkbox(checked = checked1, onCheckedChange = { checked1 = it })
-        if (checked1) {
-            LaunchedEffect(key1 = Unit) {
-                var count = 0
-                while (true) {
-                    Log.d("HomeScreen", "count = ${count++}")
-                    delay(1000)
-                }
-            }
-        }
-        val context = LocalContext.current
         Checkbox(checked = checked2, onCheckedChange = { checked2 = it })
-        if (checked2) {
-            DisposableEffect(key1 = Unit) {
-                val broadcastReceiver = object : BroadcastReceiver() {
-                    override fun onReceive(
-                        p0: Context,
-                        p1: Intent?
-                    ) {
-                        Log.d("HomeScreen", "onReceive")
-                    }
-                }
-                ContextCompat.registerReceiver(
-                    context,
-                    broadcastReceiver,
-                    IntentFilter("com.dfo.weof.sdf"),
-                    ContextCompat.RECEIVER_EXPORTED
-                )
-                onDispose {
-                    context.unregisterReceiver(broadcastReceiver)
-                }
-            }
-        }
-
-        Button(onClick = { context.sendBroadcast(Intent("com.dfo.weof.sdf")) }) {
-            Text(text = "send broadcast")
-        }
-        val scope = rememberCoroutineScope()
         Checkbox(checked = checked3, onCheckedChange = { checked3 = it })
-        if (checked3) {
-            Button(onClick = {
-                scope.launch {
-                    var count = 0
-                    while (true) {
-                        Log.d("HomeScreen", "count = ${count++}")
-                        delay(1000)
-                    }
+        if (checked1){
+            val counter by produceState(initialValue = 0) {
+                awaitDispose {
+
                 }
-            }) {
-                Text(text = "launch coroutine")
             }
+            Text(text = "count - $counter")
+        }
+        if (checked2){
+            var sliderPosition by remember { mutableStateOf(1f) }
+            Slider(
+                value = sliderPosition,
+                onValueChange = { sliderPosition = it },
+                valueRange = 0f..10f
+            )
+            TrackPostition(position = sliderPosition)
+        }
+    }
+}
+
+@Composable
+fun TrackPostition(position: Float) {
+//    val postitionState = remember {
+//        mutableStateOf(position)
+//    }
+//    postitionState.value = position
+
+    val positionState = rememberUpdatedState(position)
+    LaunchedEffect(true) {
+        while (true){
+            delay(1000)
+            Log.d("TAG", "TrackPostition: ${positionState.value}")
         }
     }
 }
