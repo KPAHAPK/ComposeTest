fafbed58be3390fa5c6beff102c54297cee3c3d3
Kran <Roman444.zaharov@gmail.com>
2025-10-08 14:24:26 +0300

    lesson 20.1

--- app/src/main/java/com/example/composetry/HomeScreen.kt
+++ app/src/main/java/com/example/composetry/HomeScreen.kt
@@ -1,28 +1,24 @@
 package com.example.composetry
 
-import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.runtime.setValue
-import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.compose
-import kotlin.math.roundToInt
 
+val LocalFontStyle = compositionLocalOf { FontStyle.Normal }
 @Composable
 fun HomeScreen(
 ) {
@@ -30,49 +26,37 @@ fun HomeScreen(
         var checked1 by remember { mutableStateOf(false) }
         var checked2 by remember { mutableStateOf(false) }
         var checked3 by remember { mutableStateOf(false) }
+        val italicState = remember { mutableStateOf(false) }
         Checkbox(checked = checked1, onCheckedChange = { checked1 = it })
         Checkbox(checked = checked2, onCheckedChange = { checked2 = it })
         Checkbox(checked = checked3, onCheckedChange = { checked3 = it })
-        if (checked1) {
-            val sliderPosition = remember { mutableStateOf(1f) }
-            MySlider(sliderPosition)
-            val roundedPosition = remember { derivedStateOf { sliderPosition.value.roundToInt() } }
-            Text(text = "position ${roundedPosition}")
-            Log.d("TAG", "HomeScreen: ${roundedPosition.value}")
+        MyCheckbox("Italic", italicState)
+
+        val fontStyle = if (italicState.value) FontStyle.Italic else FontStyle.Normal
+        CompositionLocalProvider(LocalFontStyle provides fontStyle) {
+            MyText(text = "Text 1")
+            MyText(text = "Text 2")
+            MyText(text = "Text 3")
+            MyText(text = "Text 4")
         }
-        if (checked2) {
-            val sliderPosition = remember { mutableStateOf(1f) }
-            Slider(
-                value = sliderPosition.value,
-                onValueChange = { sliderPosition.value = it },
-                valueRange = 0f..10f
-            )
-            TrackPostition(sliderPosition)
-        }
+        MyText(text = "Text 5")
     }
 }
 
 @Composable
-fun MySlider(sliderPosition: MutableState<Float>) {
-    Slider(
-        value = sliderPosition.value,
-        onValueChange = { sliderPosition.value = it },
-        valueRange = 0f..10f
-    )
-}
-
-@Composable
-fun TrackPostition(position: State<Float>) {
-    LaunchedEffect(true) {
-        val flow = snapshotFlow{position.value}
-        flow.collect {
-            Log.d("TAG", "TrackPostition: ${it}")
-
-        }
+fun MyCheckbox(text: String, checked: MutableState<Boolean>) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Checkbox(checked = checked.value, onCheckedChange = {checked.value = it})
+        Text(text = text)
     }
 }
 
-@Preview
+@Composable
+fun MyText(text: String) {
+    Text(text = text, fontStyle = LocalFontStyle.current)
+}
+
+@Preview(showBackground = true)
 @Composable
 fun HomeScreenPreview() {
     HomeScreen()
